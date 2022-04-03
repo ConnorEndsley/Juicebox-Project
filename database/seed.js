7c457925c530a398f1e6cbf3fc5617bd03eaa2e4
Connor Endsley <endsleyconnor95@gmail.com>
2022-04-03 10:50:40 -0500

    completed part 1 of juicebox!

--- database/seed.js
+++ database/seed.js
@@ -9,7 +9,8 @@ const { client,
     getUserById,
     addTagsToPost,
     createTags,
-    createPostTag } = require('./index');
+    createPostTag,
+    getPostsByTagName } = require('./index');
 
 
 // this function will call a query that will drop all tables from the database
@@ -62,10 +63,11 @@ async function createTables(){
             name VARCHAR(255) UNIQUE NOT NULL
         );
 
-        CREATE TABLE post_tags(
-            "postId" INTEGER REFERENCES posts(id) UNIQUE NOT NULL,
-            "tagId" INTEGER REFERENCES tags(id) UNIQUE NOT NULL
-        )
+        CREATE TABLE post_tags (
+            "postId" INTEGER REFERENCES posts(id),
+            "tagId" INTEGER REFERENCES tags(id),
+            UNIQUE ("postId", "tagId")
+          );
      `);
 
     console.log("Best tables build!")
@@ -92,33 +94,51 @@ async function createInitialUsers() {
     }
 }
 
-async function createInitialPosts(){
-    try{
-        const [albert, sandra, glamgal] = await getAllUsers();
-
-        await createPost({
-            authorId: albert.id,
-            title: "First Post",
-            content: "This is my first post!"
-        });
-        
-        await createPost({
-            authorId: sandra.id,
-            title:"First Post",
-            content:"This is also my first post!"
-        });
-
-        await createPost({
-            authorId: glamgal.id,
-            title: "First Post",
-            content: "Don't forget about my first post!"
-        });
-
-    } catch(error){
-        throw error
+async function createInitialPosts() {
+    try {
+      const [albert, sandra, glamgal] = await getAllUsers();
+  
+      console.log("Starting to create posts...");
+      await createPost({
+        authorId: albert.id,
+        title: "First Post",
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+        tags: ["#happy", "#youcandoanything"]
+      });
+  
+      await createPost({
+        authorId: sandra.id,
+        title: "How does this work?",
+        content: "Seriously, does this even do anything?",
+        tags: ["#happy", "#worst-day-ever"]
+      });
+  
+      await createPost({
+        authorId: glamgal.id,
+        title: "Living the Glam Life",
+        content: "Do you even? I swear that half of you are posing.",
+        tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+      });
+      console.log("Finished creating posts!");
+    } catch (error) {
+      console.log("Error creating posts!");
+      throw error;
     }
-};
-
+  }
+  
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+      await createInitialUsers();
+      await createInitialPosts();
+    } catch (error) {
+      console.log("Error during rebuildDB")
+      throw error;
+    }
+  } 
 async function createInitialTags() {
     try {
       console.log("Starting to create tags...");
@@ -151,7 +171,6 @@ async function rebuildDB() {
         await createTables();
         await createInitialUsers();
         await createInitialPosts();
-        await createInitialTags();
     } catch (error) {;
         throw error;
     }
@@ -186,11 +205,23 @@ async function testDB() {
       console.log("Calling getUserById with 1");
       const albert = await getUserById(1);
       console.log("Result:", albert);
+
+      console.log("Calling updatePost on posts[1], only updating tags");
+      const updatePostTagsResult = await updatePost(posts[1].id, {
+        tags: ["#youcandoanything", "#redfish", "#bluefish"]
+      });
+      console.log("Result:", updatePostTagsResult);
+
+      console.log("Calling getPostsByTagName with #happy");
+      const postsWithHappy = await getPostsByTagName("#happy");
+      console.log("Result:", postsWithHappy);
   
       console.log("Finished database tests!");
     } catch (error) {
       console.log("Error during testDB");
       throw error;
+
+
     }
   }
   
